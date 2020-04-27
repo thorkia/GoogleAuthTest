ca2e2146a40213bb8b8398e66a6ce304c510d1eb
Robert Maissan <robert.maissan@gmail.com>
2020-04-27 18:33:38 -0400

    Sample Project that shows .NET Core GoogleAuth is not respecting my redirect call back end point

---
+++ GoogleAuthTest.WepAPI/Controllers/ExternalController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoogleAuthTest.WepAPI.Controllers
+{
+	[Route("authentication/external")]
+	public class ExternalController : ControllerBase
+	{
+
+		[AllowAnonymous]
+		[HttpGet("google-login")]
+		public IActionResult GoogleLogin(string returnUrl)
+		{
+			returnUrl = "/Index";
+
+			var properties = new AuthenticationProperties { RedirectUri = Url.Action(nameof(GoogleLoginCallback), new { returnUrl }) };
+
+			return new ChallengeResult(GoogleDefaults.AuthenticationScheme, properties);
+		}
+
+		[AllowAnonymous]
+		[HttpPost("google-callback")]
+		public async Task<IActionResult> GoogleLoginCallback(string returnUrl = null, string remoteError = null)
+		{
+			//Here we can retrieve the claims - never gets hit
+			var result = await HttpContext.AuthenticateAsync("AuthScheme");
+
+			if (!result.Succeeded)
+			{
+				return BadRequest();
+			}
+
+			var claimsIdentity = new ClaimsIdentity("APIScheme");
+
+			claimsIdentity.AddClaims(result.Principal.Claims);
+
+			await HttpContext.SignInAsync("APIScheme", 
+			                        new ClaimsPrincipal(claimsIdentity),
+			                        new AuthenticationProperties {IsPersistent = true});
+
+
+			return LocalRedirect(returnUrl);
+		}
+
+	}
+}
---
+++ GoogleAuthTest.WepAPI/Startup.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+
+namespace GoogleAuthTest.WepAPI
+{
+	public class Startup
+	{
+		public IConfiguration Configuration { get; }
+
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+		
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{			
+			services.AddControllers();
+
+			services.AddAuthentication( option =>
+			                            {
+				                            option.DefaultScheme = "APIScheme";
+				                            option.DefaultAuthenticateScheme = "AuthScheme";
+			                            })
+			        .AddCookie("APIScheme")
+			        .AddCookie("AuthScheme")
+			        .AddGoogle(googleOptions =>
+			                   {
+				                   googleOptions.ClientId = Configuration["Authentication:Google:ClientId"];
+				                   googleOptions.ClientSecret = Configuration["Authentication:Google:ClientSecret"];
+													 googleOptions.AuthorizationEndpoint = "/authentication/external/google-login";
+													 googleOptions.CallbackPath = "/authentication/external/google-callback";
+			                   });
+
+			services.AddMvc()
+			        .AddRazorPagesOptions(o => o.Conventions.AuthorizePage("/Index"));
+
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseHttpsRedirection();
+
+			app.UseRouting();
+
+			app.UseAuthentication();
+			app.UseAuthorization();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+				endpoints.MapRazorPages();
+			});
+		}
+	}
+}
